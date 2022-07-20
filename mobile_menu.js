22f419786ab3783add9aa67b9290669c7f614038
Blossom-Aniedi <blossomaniedi@gmail.com>
2022-07-20 11:43:42 +0100

    Add javascript files to Portfolio Website Folder

---
+++ mobile_menu.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById('menu2')
+const cancel = document.getElementById('cancel')
+const nav = document.getElementById('menu')
+
+function Show(){
+  nav.classList.add('Show')
+  cancel.style.display = 'flex'
+  menu.style.display = 'none'
+}
+
+menu.addEventListener('click', Show)
+
