6dcdebdc1efd9e0f2d30411dbab387e32e785876
betse-1 <betsegaws@gmail.com>
2022-07-20 14:35:08 +0300

    add Hide function

--- mobile_menu.js
+++ mobile_menu.js
@@ -10,5 +10,9 @@ function Show(){
 }
 
 menu.addEventListener('click', Show)
-
-
+function Hide(){
+  nav.classList.remove('Show')
+  cancel.style.display = 'none'
+  menu.style.display = 'flex'
+}
+cancel.addEventListener('click', Hide)
