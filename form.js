badce8927d415a1fb9531a1089f3cb2f7660094c
Blossom-Aniedi <blossomaniedi@gmail.com>
2022-07-23 09:50:59 +0100

    Add Popup Functionality

--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 const displayMsg = document.getElementById('form_message_error');
 const form = document.getElementById('input');
 const email = document.getElementById('email');
+const formData = JSON.parse(localStorage.getItem('data'));
 
 form.addEventListener('submit', (event) => {
   if (email.value !== email.value.toLowerCase()) {
@@ -10,5 +11,11 @@ form.addEventListener('submit', (event) => {
     displayMsg.textContent = 'Your email address should be in lowercase';
   } else {
     displayMsg.style.visibility = 'hidden';
+    const formData = {
+      name: form.value,
+      email: email.value,
+      message: displayMsg.value,
+    };
+    localStorage.setItem('data', JSON.stringify(data));
   }
 });
